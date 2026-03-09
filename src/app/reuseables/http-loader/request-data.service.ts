058364ad4a97f78702594f8e5f85ed7e3484aba1
Daniel <onyemordidaniel@gmail.com>
2026-03-09 23:13:21 +0100

    all

--- src/app/reuseables/http-loader/request-data.service.ts
+++ src/app/reuseables/http-loader/request-data.service.ts
@@ -5,8 +5,8 @@ import { timeout, catchError } from 'rxjs/operators';
 // import {  } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class RequestDataService {
-  private baseUrl = 'http://127.0.0.1:8000/api'; // Replace with your API endpoint
-  // private baseUrl = 'https://smgfb-71e344e74163.herokuapp.com/api'; // Replace with your API endpoint
+  // private baseUrl = 'http://127.0.0.1:8000/api'; // Replace with your API endpoint
+  private baseUrl = 'https://vi01-ee30f1c935bd.herokuapp.com/api'; // Replace with your API endpoint
 // dfg
   constructor(
     private http: HttpClient,
