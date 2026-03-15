e084fc85e6f13374f63652cc8ba288e48af9c181
Daniel <onyemordidaniel@gmail.com>
2026-03-15 01:53:36 +0100

    all

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,10 +5,11 @@ import { StoreDataService } from '../../reuseables/http-loader/store-data.servic
 import { CurrencyConverterPipe } from '../../reuseables/pipes/currency-converter.pipe';
 import { AuthService } from '../../reuseables/auth/auth.service';
 import { QuickNavService } from '../../reuseables/services/quick-nav.service';
+import { NotiPopupComponent } from "../../noti-popup/noti-popup.component";
 
 @Component({
   selector: 'app-header',
-  imports: [CurrencyConverterPipe, CommonModule],
+  imports: [CurrencyConverterPipe, CommonModule, NotiPopupComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
